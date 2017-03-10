62022a74a2b6fa12fdae521a7e71636f42a7f2bb
Wang <gavin19950511@gmail.com>
2017-03-10 19:04:17 +0800

    Update GPIO24.py
    add attributes :PowerState ,to  show state of gpio pin

--- FaceDetect_GPIO/GPIO24.py
+++ FaceDetect_GPIO/GPIO24.py
@@ -24,37 +24,55 @@ class GpioForLights:
 
 		#initialization : Pin off
 		self.off()
-
-		#alert text	
 		
+		#attributes
+		#Power State , 0 is off and 1 is on
+		self.PowerState = 0
+		
+		#alert text		
 		if self.AlertOn:
 			print "Setup Pin "+str(self.ledPin)+" is OutPut..."
 
 
 	def on(self):		
-  		GPIO.output(self.ledPin, True)
+  		# enable power
+		GPIO.output(self.ledPin, True)
+		
+		# set Power Satae is 1 (on)
+		self.PowerState = 1
 		
 		#alert text
 		if self.AlertOn:
 			print str(self.ledPin)+" pin Set Output On"
 	def off(self):		
-  		GPIO.output(self.ledPin, False)
+  		#disable power
+		GPIO.output(self.ledPin, False)
+		
+		# set Power Satae is 0 (off)
+		self.PowerState = 0
 		
 		#alert text
 		if self.AlertOn:
 			print str(self.ledPin)+" pin Set Output Off"
 	
 	def flash(self,rtime=3,SleepTime=1):
+		#**** Just a Test tool 
 		print str(self.ledPin)+" Pin Starting flash... "
 		for i in range(rtime):
 
 		  print "Set Output True"
-		  GPIO.output(self.ledPin, False)
-		  time.sleep(SleepTime)
-
-		  print "Set Output False"
 		  GPIO.output(self.ledPin, True)
 		  time.sleep(SleepTime)
+		
+		  # set Power Satae is 1 (on)
+		  self.PowerState = 1
+
+		  print "Set Output False"
+		  GPIO.output(self.ledPin, False)
+		  time.sleep(SleepTime)
+			
+                  # set Power Satae is 0 (off)
+		  self.PowerState = 0
 		#self.clean()
 		#alert text
 		if self.AlertOn:
